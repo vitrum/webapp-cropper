0b9c2ebfed4a15f4efabcb38f04868d138a6344a
vitrum.zhu <vitrum.cn@gmail.com>
2014-01-27 17:54:03 +0800

    update javascript code for demo case

--- demo/app/scripts/main.js
+++ demo/app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+Hammer.plugins.fakeMultitouch();
+
+var element = document.getElementById('test_el');
+var hammertime = Hammer(element).on("touch", function(event) {
+  // console.log(WACropper);
+  var myCropper = new WACropper(element, {
+    mouseWheel: true,
+    scrollbars: true,
+    skin: 'base'
+  });
+});
