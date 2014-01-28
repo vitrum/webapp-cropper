578a27fc9494445797c1c79cf89e141680653316
vitrum.zhu <vitrum.cn@gmail.com>
2014-01-28 16:41:11 +0800

    add lib

--- demo/app/scripts/main.js
+++ demo/app/scripts/main.js
@@ -3,9 +3,11 @@ Hammer.plugins.fakeMultitouch();
 var element = document.getElementById('test_el');
 var hammertime = Hammer(element).on("touch", function(event) {
   // console.log(WACropper);
-  var myCropper = new WACropper(element, {
-    mouseWheel: true,
-    scrollbars: true,
-    skin: 'base'
-  });
+
+});
+
+var myCropper = new WACropper(element, {
+  mouseWheel: true,
+  scrollbars: true,
+  skin: 'base'
 });
